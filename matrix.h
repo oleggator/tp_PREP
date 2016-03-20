69dff6313c31691e8e265dc3738de41aa90b9bc9
Serg Titaevskiy <Titaevskiy.S@gmail.com>
2016-03-20 17:08:20 +0300

    Add the getter for columns and rows

--- matrix.h
+++ matrix.h
@@ -12,5 +12,7 @@ Matrix* create_matrix(int row, int col);
 void free_matrix(Matrix* matrix);
 double get_elem(Matrix* matrix, int row, int col);
 void set_elem(Matrix* matrix, int row, int col, double val);
+int get_rows(Matrix* matrix);
+int get_cols(Matrix* matrix);
 
 #endif //_MATRIX_H_
